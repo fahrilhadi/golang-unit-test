d440bc1f44c9c9657e052f5cc0414aa8d7b69b34
Fahril Hadi <fahrilhadi77@gmail.com>
2025-02-07 21:13:27 +0700

    menggagalkan test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T)  {
 	result := HelloWorld("Fahril")
@@ -14,4 +17,26 @@ func TestHelloWorld(t *testing.T)  {
 	// gunakan perintah go test -v (melihat detail function test apa saja yang sudah di running)
 	// gunakan perintah go test -v -run TestNamaFunction (memilih function unit test mana yang ingin di running)
 	// gunakan perintah go test -v ./... (menjalankan semua unit test dari top folder module nya)
+}
+
+func TestHelloWorldHadi(t *testing.T)  {
+	result := HelloWorld("Hadi")
+
+	if result != "Hello Hadi" {
+		// error
+		t.Error("Result must be 'Hello Hadi'")
+	}
+
+	fmt.Println("TestHelloWorldHadi Done")
+}
+
+func TestHelloWorldAbu(t *testing.T)  {
+	result := HelloWorld("Abu")
+
+	if result != "Hello Abu" {
+		// error
+		t.Fatal("Result must be 'Hello Abu'")
+	}
+
+	fmt.Println("TestHelloWorldAbu Done")
 }
