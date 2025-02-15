b4dca016923a579ba2535c830518a1e3f783f872
Fahril Hadi <fahrilhadi77@gmail.com>
2025-02-15 22:58:30 +0700

    sub benchmark

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkSub(b *testing.B)  {
+	b.Run("Fahril", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Fahril")
+		}
+	})
+	b.Run("Hadi", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Hadi")
+		}
+	})
+}
+
 func BenchmarkHelloWorld(b *testing.B)  {
 	for i := 0; i < b.N; i++ {
 		HelloWorld("Fahril")
